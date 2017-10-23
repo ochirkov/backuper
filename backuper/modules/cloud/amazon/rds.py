4b6f39ebd123ddcc3726bd9e0fecb60ce7443672
blckjack <1@0.rubley>
2017-10-23 20:11:45 +0300

    add restore method comment

--- backuper/modules/cloud/amazon/rds.py
+++ backuper/modules/cloud/amazon/rds.py
@@ -250,7 +250,9 @@ class Main(object):
         if self.kwargs['action'] == 'restore':
             restore_from_snapshot = self.restore_from_snapshot(self.config()['a_region'])
             print("Instance creation is in progress, keep calm while it takes another minute...")
+
             i = 0
             while i != 'available':
                 i = self.instance_is_available(self.config()['a_region'])
                 sleep(60)
+            print("Instance was restored.")
